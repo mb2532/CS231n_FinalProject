a970aa5ea6513f5a0273108d5d8b6a7861aec66c
megan <40206838+mb2532@users.noreply.github.com>
2022-05-18 18:01:31 -0700

    fix gif save

--- ConvLSTM/ConvLSTM_training.py
+++ ConvLSTM/ConvLSTM_training.py
@@ -145,21 +145,21 @@ for epoch in range(1, num_epochs+1):
     with io.BytesIO() as gif:
         new_input = np.array(input[0, :, :, :, :]) * 255.0
         new_input = new_input.transpose(1, 2, 3, 0)
-        imageio.mimsave(gif, new_input, "GIF", fps = 5)    
+        imageio.mimsave(gif, new_input, "input_gif_epoch" + str(epoch), fps = 5)    
         #input_gif = gif.getvalue()
     # Write target video as gif
     with io.BytesIO() as gif:
         new_target = np.array(target[0, :, :, :].unsqueeze(0))* 255.0
         new_target = new_target.transpose(0, 2, 3, 1)
         print(target.shape)
-        imageio.mimsave(gif, new_target, "GIF", fps = 5)    
+        imageio.mimsave(gif, new_target, "target_gif_epoch" + str(epoch), fps = 5)    
         #target_gif = gif.getvalue()
 
     # Write output video as gif
     with io.BytesIO() as gif:
         new_output = output[0, :, :, :].unsqueeze(0).detach().numpy()* 255.0
         new_output = new_output.transpose(0, 2, 3, 1)
-        imageio.mimsave(gif, new_output, "GIF", fps = 5)    
+        imageio.mimsave(gif, new_output, "output_gif_epoch" + str(epoch), fps = 5)    
         #output_gif = gif.getvalue()
 
 
